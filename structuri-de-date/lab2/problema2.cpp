cad9f16918ca9b799e879dfd03a333bdb1eaf127
Ioan <me@smuwn.xyz>
2019-03-05 13:50:50 +0200

    Add files via upload

---
+++ structuri-de-date/lab2/problema2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+	int lung, temp;
+	
+	cout << "\nNumar linii:";
+	cin >> lung;
+	
+	int matrice[lung];
+	
+	for(int i = 0; i < lung; i++){
+		for(int j = 0; j < lung; j++){
+			cout << "matrice[" << i "][" << j << "] = ";
+			cin >> matrice[i][j];
+		}
+	}
+	
+	for(int a = 0; a < lung / 2; a++){
+		temp = matrice[a][a];
+		for(int i = a; i < lung - a - 1; i++) matrice[a][i] = matrice[a][i + 1]; 
+		for(int i = a; i < lung - a - 1; i++) matrice[i][lung - a];
+		for(int i = lung - a - 1; i >= a; i--) matrice[]
+	}
+
+}
