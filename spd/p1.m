ebb5a5ef27b3fb90544ef15d897d7f15c2bb6d0b
thirdless <me@smuwn.xyz>
2019-11-28 15:38:56 +0200

    Create p1.m

---
+++ spd/p1.m
@@ -0,0 +1,28 @@
+n = input('nr de valori = ');
+x = zeros(1, n);
+p = zeros(1, n);
+
+for i = 1:n
+    x(i) = i;
+    p(i) = 1/n;
+end
+
+x
+p
+
+subplot(2,1,1);
+hold on
+grid on
+plot(x(1:n), p(1:n), '*r')
+
+subplot(2,1,2)
+hold on
+grid on
+frep = 0;
+plot([x(1)-2 x(1)], [frep frep])
+
+for i=1:n
+    frep = frep + p(i);
+    plot([i, i+1], [frep frep]);
+end
+plot([x(n) x(n)+2], [frep frep]);
