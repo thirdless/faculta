788fb180689c1b743a75fbab1e22d803e3eb9036
thirdless <me@smuwn.xyz>
2020-03-02 10:29:39 +0200

    Create p1.v

---
+++ proiectarea-sistemelor/lab3/p1.v
@@ -0,0 +1,26 @@
+module gen_clk;
+
+reg clk1;
+reg clk2;
+reg clk3;
+
+inital
+begin
+	#0 clk1 = 1'b0;
+	forever #5 clk1 = ~clk1;
+end
+
+intial
+begin
+	#0 clk2 = 1'b1;
+	forever #10 clk2 = ~clk2;
+end
+
+initial
+begin
+	#0 clk3 = 1'b1;
+	#7 clk3 = 1'b0;
+	forever #20 clk3 = ~clk3;
+end
+
+endmodule
