3a0311fefd7180fcbd6d5f3dbe63679a33936ada
thirdless <me@smuwn.xyz>
2020-03-09 19:50:00 +0200

    Create P3.java

---
+++ proiectare-aplicatii-orientate/lab4/p3/P3.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.io.*;
+
+public class P3{
+
+    public static void main(String[] args) throws IOException
+    {
+        Matrix temp = new Matrix();
+        System.out.print(temp.toString());
+    }
+}
