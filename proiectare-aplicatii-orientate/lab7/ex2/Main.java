173adc5fc2d0d10d85de445eee9f612a3a00b0a2
thirdless <me@smuwn.xyz>
2020-04-01 17:19:55 +0300

    Update Main.java

--- proiectare-aplicatii-orientate/lab7/ex2/Main.java
+++ proiectare-aplicatii-orientate/lab7/ex2/Main.java
@@ -1,1 +1,18 @@
+package com.company;
 
+import com.company.Pizza.Pizza;
+import com.company.PizzaFactory.PizzaFactory;
+import com.company.Pizza.PizzaType;
+
+public class Main {
+
+    public static void main(String[] args) {
+        PizzaFactory pizzas[] = {
+                new PizzaFactory(PizzaType.BACON, 35, 25),
+                new PizzaFactory(PizzaType.QUATRO, 20, 15),
+                new PizzaFactory(PizzaType.CHEESE, 50, 40),
+                new PizzaFactory(PizzaType.SALAMI, 40, 35)
+        };
+        for(int i = 0; i < 4; i++) System.out.println(pizzas[i].toString());
+    }
+}
